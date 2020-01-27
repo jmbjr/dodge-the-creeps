b866d6ac33319194858186a295d0373032680477
John Boyle <boylejm@gmail.com>
2020-01-26 17:28:22 -0800

    rotate player when moving left and right.
    had to also rotate the collision shape too

--- Player.gd
+++ Player.gd
@@ -12,7 +12,7 @@ func _ready():
 	
 func _process(delta):
 	velocity = get_direction()
-	
+	var new_rotation
 	if velocity.length() > 0:
 		$AnimatedSprite.play()
 		velocity = velocity.normalized() * SPEED
@@ -24,15 +24,22 @@ func _process(delta):
 	position.y = clamp(position.y, 0, screensize.y)
 	
 	if velocity.x != 0:
+		new_rotation = PI/2 * sign(velocity.x)
 		$AnimatedSprite.animation = "right"
+		rotate_player(new_rotation)
 		$AnimatedSprite.flip_v = false
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
+		new_rotation = 0
 		$AnimatedSprite.animation = "up"
+		rotate_player(new_rotation)
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.flip_v = velocity.y > 0
 		
-
+func rotate_player(new_rotation) -> void:
+	$AnimatedSprite.rotation = new_rotation
+	$CollisionShape2D.rotation = new_rotation
+	
 func get_direction() -> Vector2:
     return Vector2(
         Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
